d7da7df0c8992bd04875b795002dbd028e817867
rtblast70 <ryanj.tsai@gmail.com>
2022-12-03 00:28:28 -0800

    added RDDs of teams w attributes and teams w record

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -43,24 +43,21 @@ object Main {
 
     case class Player(id: Int, name: String, height: Double, weight: Int);
 
-    case class Teams(id: Int, name: String);
-
-    //TeamAttributes(id, date, buSpeed, buDribbling, buPassing, buPositioning,
-    //              ccPassing, ccCrossing, ccShooting, ccPositioning, dPressure, dAggresion, dWidth, dLine)
+    case class Team(id: Int, name: String);
     case class TeamAttributes(id: Int, buSpeed: String, buDribbling: String, buPassing: String, buPositioning: String,
     ccPassing: String, ccCrossing: String, ccShooting: String, ccPositioning: String, dPressure: String, dAggresion: String, dWidth: String, dLine: String)
 
+    // RDD of data using custom classes
 
-    val MatchData = sc.textFile("./data/Matches.csv").map{line => {
+    val matchData = sc.textFile("./data/Matches.csv").map{line => {
       var temp = line.split(",");
       Matches(temp(0).toInt, temp(1).toInt, temp(2).toInt, temp(3).toInt, temp(4).toInt, temp(5).toInt);
     }}
 
-    val TeamData = sc.textFile("./data/Teams.csv").map(line => {
-      Teams(line.split(",")(0).toInt, line.split(",")(1))
+    val teamData = sc.textFile("./data/Teams.csv").map(line => {
+      Team(line.split(",")(0).toInt, line.split(",")(1))
     })
 
-    //262
     val teamAttributeData = sc.textFile("./data/Team_Attributes.csv").map(line => {
       var temp = line.split(",")
       var date = temp(1).replaceAll("\"", "").split("-")(0).toInt
@@ -70,10 +67,46 @@ object Main {
       v.toList(0)
     }}
 
-    println(teamAttributeData.count());
-
-    val leagueDate = sc.textFile("./data/Leagues.csv").map(line => {
+    val leagueData = sc.textFile("./data/Leagues.csv").map(line => {
       League(line.split(",")(0).toInt, line.split(",")(1))
+    })
+
+    // RDD from joining raw data
+
+    // teams w associated team attributes
+    // (Team(), TeamAttributes())
+    // (Team(9825,Arsenal),TeamAttributes(9825,Balanced,Normal,Short,Organised,Safe,Normal,Normal,Free Form,Medium,Press,Normal,Cover))
+    val teamWithAttributes = teamData.map(x => (x.id, x.name)).leftOuterJoin(teamAttributeData.map(x => (x.id, x)))
+      .map{case (id, (name, attr)) => {
+      (Team(id, name), attr.getOrElse(None))
+    }}
+
+    // determine if a team is home or away
+    // Matches, Int -> Int
+    // 1 = home ; -1 = away
+    def findHomeAway(m: Matches, id: Int): Int = if(m.homeID == id) 1 else -1;
+
+    // find outcome of match given team
+    // Matches, Int -> Int
+    // 1 = win ; -1 = loss ; 0 = draw
+    def calculateMatchOutcome(m: Matches, team: Int): Int = {
+      var outcome = findHomeAway(m, team) * (m.homeGoals - m.awayGoals)
+      if(outcome > 0) 1 else if(outcome < 0) -1 else 0
+    }
+
+    // teams with w/d/l ratio
+    // (Team(), List(w, d, l))
+    // (Team(9825,Arsenal),List(20, 11, 7))
+    val teamWithRecord = sc.parallelize(matchData.map{x => {
+      List((x.homeID, x), (x.awayID, x))
+    }}.collect().toList.flatten).rightOuterJoin(teamData.map{x =>{
+      (x.id, x)
+    } }).map{case (_, (m, team)) => {
+      (team, m.getOrElse(None))
+    }}.filter(_._2 != None).map{case (t, m) => {
+      (t, (calculateMatchOutcome(m.asInstanceOf[Matches], t.id), 1))
+    }}.groupByKey().mapValues(m => {
+      m.groupBy(_._1).mapValues(_.size).toList.sortBy(_._1 * -1).map(_._2);
     })
 
   }
